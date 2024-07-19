1dc4af10bdb5ecdc66b0d8f0d1529d22e16f3e45
cassioglay <cassio.glay#locktec.com.br>
2024-07-19 16:00:58 -0300

    feat: Logger - add handler logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/cassioglay/opportunities/router"
+import (
+	"github.com/cassioglay/opportunities/config"
+	"github.com/cassioglay/opportunities/router"
+)
+
+var logger *config.Logger
 
 func main() {
+
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
