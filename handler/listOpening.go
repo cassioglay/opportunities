81ae7bbbc75122eb7bb05e9cbc4ca2bd7627f36a
cassioglay <cassio.glay#locktec.com.br>
2024-07-22 17:19:31 -0300

    doc: Documentation - add docs wiht swagger

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary List opening
+// @Description list all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 
